d927c7f3fa93a66fb56ae696246ab8378a4f47c3
Lucas <lucaswar05@gmail.com>
2024-08-10 14:02:19 -0300

    Ex03 - Primeiro commit

---
+++ Exercicio03/src/view/ClassePrincipal.java
@@ -0,0 +1,8 @@
+package view;
+import controller.ClasseMetodos;
+public class ClassePrincipal {
+    public static void main(String[] args) {
+        // Instancia de classe:
+        ClasseMetodos metodos = new ClasseMetodos();
+    }
+}
