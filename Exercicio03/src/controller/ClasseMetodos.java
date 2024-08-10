965929550896c631ac5e7786ed5f68a865430db6
Lucas <lucaswar05@gmail.com>
2024-08-10 15:16:55 -0300

    Ultima atualização, Ex03 - Final

--- Exercicio03/src/controller/ClasseMetodos.java
+++ Exercicio03/src/controller/ClasseMetodos.java
@@ -1,7 +1,44 @@
 package controller;
+import javax.swing.JOptionPane;
 public class ClasseMetodos {
     // Criar construtor:
     public ClasseMetodos(){
         super();
+    }
+    // Retorna um valor menor que 100 digitado pelo usuário para o tamanho do vetor:
+    public int tamanhoVet(){
+        // Definir variaveis:
+        int tamanho;
+        while(true){
+            tamanho = Integer.parseInt(JOptionPane.showInputDialog("Digite o tamanho do vetor: "));
+            // validação do valor:
+            if (tamanho < 101){
+                break;
+            } else {
+                JOptionPane.showMessageDialog(null, "Valor inválido, digite novamente."); 
+            }
+        }
+        // Saída de dados:
+        return tamanho;
+    }
+    // Preenche vetor:
+    public int[] preencheVet(int[] vet){
+        // Entrada de dados:
+        for(int cto = 0 ; cto < vet.length ; cto ++){
+            vet[cto] = Integer.parseInt(JOptionPane.showInputDialog("Digite o " + (cto + 1) + "º valor:"));
+        }
+        // Saída de dados:
+        return vet;
+    }
+    // Verifica o valor se é ímpar ou par multiplo de 10:
+    public void verificaValor(int[] vet){
+        for(int num : vet){
+            // Processamento + saída de dados
+            if (num % 2 != 0) {
+                System.out.println("O número: " + num + ", é ímpar.");
+            } else if (num % 2 == 0 && num % 10 == 0) {
+                System.out.println("O número: " + num + ", é par e múltiplo de 10.");
+            }
+        }
     }
 }
--- Exercicio03/src/view/ClassePrincipal.java
+++ Exercicio03/src/view/ClassePrincipal.java
@@ -4,5 +4,11 @@ public class ClassePrincipal {
     public static void main(String[] args) {
         // Instancia de classe:
         ClasseMetodos metodos = new ClasseMetodos();
+        // Definir variáveis:
+        int[] vetor = new int[metodos.tamanhoVet()];
+        // Entrada de dados:
+        metodos.preencheVet(vetor);
+        // Processamento + saída de dados:
+        metodos.verificaValor(vetor);
     }
 }
